2cf54801b627baa7f9858972395295246e3c5e56
Katica Rankovic <katica.rankovic@enreach.com>
2020-12-06 18:38:25 +0100

    diff salary functionality

--- src/app/components/empl-list/empl-list.component.ts
+++ src/app/components/empl-list/empl-list.component.ts
@@ -61,9 +61,9 @@ export class EmplListComponent implements OnInit {
     },
     {
       id: 5,
-      name: "Aleksandra",
+      name: "Alexandra",
       lastName: "Jones",
-      fullName: "Aleksandra Jones",
+      fullName: "Alexandra Jones",
       picture: "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=300",
       salary: 5654,
       onVac : true,
--- src/app/components/empl-preview/empl-preview.component.ts
+++ src/app/components/empl-preview/empl-preview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { EmplServiceService } from 'src/app/services/empl-service.service';
 
 @Component({
@@ -9,16 +9,14 @@ import { EmplServiceService } from 'src/app/services/empl-service.service';
 export class EmplPreviewComponent implements OnInit {
   employeeProfile:any = "";
   show:boolean = false;
-  constructor(private service:EmplServiceService) { }
+  constructor(private service:EmplServiceService) {
+  }
 
   ngOnInit(): void {
     this.service.empPreview$.subscribe(emp => this.employeeProfile = emp);
   }
-
-  showSalary(){
-    this.show = true;
-    setTimeout(e=>{
-      this.show = false
-    }, 1500)
+  showSalary(event){
+    event.target.innerText = (!this.show) ? 'Hide salary' : 'Show salary';
+    this.show = !this.show;
   }
 }
